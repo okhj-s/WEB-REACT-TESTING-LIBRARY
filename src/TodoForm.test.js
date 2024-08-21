88cae2a9e08ab28cc0f53987493e43cd236e09a2
okhj159 <okhj159@gmail.com>
2024-08-21 13:57:02 +0900

    test: submit test code 추가

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -18,4 +18,20 @@ describe('<TodoForm />', () => {
         });
         expect(input).toHaveAttribute('value', 'TDD 배우기')
     })
+    it('calls onInsert and clears input', () => {
+        const onInsert = jest.fn()
+        render(<TodoForm onInsert={onInsert} />)
+        const input = screen.getByPlaceholderText('할 일을 입력하세요')
+        const button = screen.getByText('등록')
+        // 수정하고
+        fireEvent.change(input, {
+            target: {
+                value: 'TDD 배우기'
+            }
+        });
+        // 버튼 클릭
+        fireEvent.click(button)
+        expect(onInsert).toHaveBeenCalledWith('TDD 배우기') // onInsert가 'TDD 배우기' 파라미터가 호출됐어야함
+        expect(input).toHaveAttribute('value', '') // input이 비워져야함
+    })
 })
